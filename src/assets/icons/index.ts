2833d0faa46721a9165de57fbac61eaa4b42db68
M Jazeb Javed <jazebjaved52@gmail.com>
2024-11-19 01:17:14 +0500

    feat: add SplashScreen component and SportEazeLogo icon; update Navigationcontainer for initial app loading state

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,6 +2,7 @@ import {CommentIcon} from './svgs/CommentIcon';
 import {HeartIcon} from './svgs/HeartIcon';
 import {ImagePlaceholderIcon} from './svgs/ImagePlaceholderIcon';
 import {ShareIcon} from './svgs/ShareIcon';
+import {SportEazeLogo} from './svgs/SportEazeLogo';
 import {UserPlaceholderIcon} from './svgs/UserPlaceholderIcon';
 
 export {
@@ -10,4 +11,5 @@ export {
   CommentIcon,
   ShareIcon,
   ImagePlaceholderIcon,
+  SportEazeLogo,
 };
