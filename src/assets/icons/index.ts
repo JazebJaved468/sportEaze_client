413fb108d7d4d7f9bf0ba42b43577584ba7addd9
M Jazeb Javed <jazebjaved52@gmail.com>
2025-01-01 02:43:57 +0500

    feat: implement onboarding screen and add new color constants; include new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,7 +4,9 @@ import {HeartIcon} from './svgs/HeartIcon';
 import {ImagePlaceholderIcon} from './svgs/ImagePlaceholderIcon';
 import {MessageSendIcon} from './svgs/MessageSendIcon';
 import {MuteIcon} from './svgs/MuteIcon';
+import {PlayerModelIcon} from './svgs/PlayerModelIcon';
 import {PlayIcon} from './svgs/PlayIcon';
+import {ScarIcon} from './svgs/ScarIcon';
 import {ShareIcon} from './svgs/ShareIcon';
 import {SportEazeLogo} from './svgs/SportEazeLogo';
 import {UnMuteIcon} from './svgs/UnMuteIcon';
@@ -22,4 +24,6 @@ export {
   SportEazeLogo,
   UnMuteIcon,
   UserPlaceholderIcon,
+  ScarIcon,
+  PlayerModelIcon,
 };
