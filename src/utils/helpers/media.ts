191cce612f9ae3a24c2c3cddb03627c7418bd214
M Jazeb Javed <jazebjaved52@gmail.com>
2025-03-18 02:07:59 +0500

    feat: add getMediaType helper function to classify media types based on MIME type

---
+++ src/utils/helpers/media.ts
@@ -0,0 +1,27 @@
+import {MediaType} from '../../constants/enums';
+
+export const getMediaType = (
+  mimeType: string,
+): MediaType.IMAGE | MediaType.VIDEO | MediaType.DOCUMENT => {
+  if (mimeType.startsWith('image/')) {
+    return MediaType.IMAGE;
+  }
+  if (mimeType.startsWith('video/')) {
+    return MediaType.VIDEO;
+  }
+  if (
+    [
+      'application/pdf',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/vnd.ms-powerpoint',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'text/plain',
+    ].includes(mimeType)
+  ) {
+    return MediaType.DOCUMENT;
+  }
+  return MediaType.DOCUMENT; // Default case if MIME type doesn't match
+};
