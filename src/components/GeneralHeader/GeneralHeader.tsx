48ff102baa46a189836a3511b784d85d4dca658b
M Jazeb Javed <jazebjaved52@gmail.com>
2025-03-17 10:37:47 +0500

    feat: update GeneralHeader to use dynamic font styling and improve right element rendering

--- src/components/GeneralHeader/GeneralHeader.tsx
+++ src/components/GeneralHeader/GeneralHeader.tsx
@@ -9,6 +9,7 @@ import {RegisterPage} from '../../modules/Core/Auth/Register';
 import {LoginPage} from '../../modules/Core/Auth/Login';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {BackIcon, LogoutIcon, UserPlaceholderIcon} from '../../assets/icons';
+import {fontBold} from '../../styles/fonts';
 
 type GeneralHeaderProps = {
   title?: string;
@@ -63,7 +64,7 @@ export const GeneralHeader: React.FC<GeneralHeaderProps> = ({
       </View>
 
       {showTitle ? (
-        <Text style={[styles.title, {color: textColor}]}>{title}</Text>
+        <Text style={[fontBold(20, textColor), styles.title]}>{title}</Text>
       ) : null}
 
       {/* <Text style={[styles.textStyle, {color: textColor}]}>
@@ -75,7 +76,9 @@ export const GeneralHeader: React.FC<GeneralHeaderProps> = ({
 
       <View style={{position: 'absolute', right: 16}}>
         {showRightElement ? (
-          isLoggedIn ? (
+          rightElement ? (
+            rightElement
+          ) : isLoggedIn ? (
             <View style={{flexDirection: 'row', gap: 10, alignItems: 'center'}}>
               <TouchableOpacity
                 activeOpacity={0.5}
@@ -159,7 +162,5 @@ const styles = StyleSheet.create({
 
   title: {
     textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
   },
 });
