0456903fc69d5278fbf1d4daca46f3d9c5649485
M Jazeb Javed <jazebjaved52@gmail.com>
2025-03-23 03:09:40 +0500

    feat: update socket service to connect to new WebSocket URL and handle new comment events

--- src/store/socket/socket.service.ts
+++ src/store/socket/socket.service.ts
@@ -1,7 +1,9 @@
 import {io, Socket} from 'socket.io-client';
 import {store} from '../store';
+import {Sport} from '../../types/core/core.type';
 
-const SOCKET_URL = 'http://192.168.100.18:3000'; // Replace with your backend URL
+// const SOCKET_URL = 'http://192.168.100.18:3000'; // Replace with your backend URL
+const SOCKET_URL = 'ws://192.168.100.18:3000/post-comments'; // Replace with your backend URL
 
 let socket: Socket | null = null;
 
@@ -22,6 +24,12 @@ export const connectSocket = () => {
     });
 
     socket.on('disconnect', () => console.log('Socket Disconnected!'));
+
+    socket.on('newComment', data => {
+      console.log('Commented', data);
+    });
+
+    socket.emit('joinPost', 'c3b8e818-6cc7-40f2-9e30-de0992765fa1');
   }
 
   return socket;
