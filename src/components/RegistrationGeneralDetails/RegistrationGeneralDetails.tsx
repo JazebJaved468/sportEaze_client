cbf3ed8080a8874f27453496c9b239283ef254be
M Jazeb Javed <jazebjaved52@gmail.com>
2025-03-24 07:20:06 +0500

    feat: add RegistrationGeneralDetails component for user registration flow

---
+++ src/components/RegistrationGeneralDetails/RegistrationGeneralDetails.tsx
@@ -0,0 +1,301 @@
+import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React, {Dispatch, SetStateAction, useState} from 'react';
+import {RegisterFanFormData} from '../../modules/Fan/FanRegistrationDetails/FanRegistrationDetails';
+import {RegisterPlayerFormData} from '../../modules/Player/PlayerRegistrationDetails/PlayerRegistrationDetails';
+import {useForm, Controller} from 'react-hook-form';
+import {CameraIcon, UserPlaceholderIcon} from '../../assets/icons';
+import {appColors} from '../../constants/colors';
+import {useTextColor} from '../../utils/customHooks/colorHooks';
+import {useAppSelector} from '../../utils/customHooks/storeHooks';
+import {
+  openImageCamera,
+  openImagePicker,
+} from '../../utils/helpers/mediaPicker';
+import {prefixWithAtSymbol} from '../../utils/helpers/string';
+import {CustomTextInputField} from '../CustomInputField';
+import {DateTimePicker} from '../DateTimePicker';
+import {GenderPicker} from '../GenderPicker';
+import {PulseEffect} from '../PulseEffect';
+
+import {Image as ImageType} from 'react-native-image-crop-picker';
+import {Button, View} from 'native-base';
+import {BUTTON_BORDER_RADIUS} from '../../constants/styles';
+type RegistrationGeneralDetailsProps = {
+  setRegistrationStep: Dispatch<SetStateAction<number>>;
+  setFormData: Dispatch<
+    SetStateAction<RegisterFanFormData | RegisterPlayerFormData>
+  >;
+  formData: RegisterFanFormData | RegisterPlayerFormData;
+  selectedImage: ImageType | null;
+  setSelectedImage: Dispatch<SetStateAction<ImageType | null>>;
+};
+
+type GeneralDetailsFormData = {
+  fullName: string;
+  username: string;
+  dob: string;
+  gender: number;
+};
+
+const RegistrationGeneralDetails: React.FC<RegistrationGeneralDetailsProps> = ({
+  setRegistrationStep,
+  setFormData,
+  formData,
+  selectedImage,
+  setSelectedImage,
+}) => {
+  const {user} = useAppSelector(state => state.auth);
+
+  const textColor = useTextColor();
+
+  const {
+    handleSubmit,
+    control,
+
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      fullName: user?.fullName || formData.fullName || '',
+      username: user?.username || formData.username || '',
+      dob: formData.dob || (undefined as string | undefined),
+      gender: formData.gender as number | undefined,
+    },
+  });
+
+  const onSubmit = async (data: GeneralDetailsFormData) => {
+    try {
+      console.log('data', data);
+      setFormData(prev => ({
+        ...prev,
+        ...data,
+      }));
+      setRegistrationStep(2);
+      return;
+    } catch (error) {
+      console.log(
+        '-------xxxxxx----------Error while Fan details : FanRegistrationDetails.tsx',
+        error,
+      );
+    }
+  };
+
+  const selectImage = async ({
+    useCamera = false, // By Default Gallery will open
+  }: {
+    useCamera?: boolean;
+  }) => {
+    const image = useCamera
+      ? await openImageCamera()
+      : await openImagePicker(false);
+
+    if (image.length > 0) {
+      setSelectedImage(image[0]);
+    }
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      {/* Profile Pic  */}
+      <TouchableOpacity
+        onPress={async () => {
+          await selectImage({useCamera: false});
+        }}
+        activeOpacity={0.5}>
+        <View
+          style={{
+            marginBottom: 26,
+
+            alignSelf: 'center',
+            width: 160,
+            height: 160,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              right: 16,
+              bottom: 16,
+              zIndex: 1,
+              borderRadius: 12,
+              paddingHorizontal: 8,
+              paddingVertical: 8,
+              backgroundColor: appColors.warmRed,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <CameraIcon width={16} height={16} color={appColors.white} />
+          </View>
+
+          <View
+            style={{
+              backgroundColor: appColors.whisperGray,
+              width: 140,
+              height: 140,
+              borderRadius: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              overflow: 'hidden',
+            }}>
+            {selectedImage === null ? (
+              <UserPlaceholderIcon width={100} height={100} color={textColor} />
+            ) : (
+              <Image
+                source={{uri: selectedImage.path}}
+                style={{width: 140, height: 140}}
+              />
+            )}
+          </View>
+        </View>
+      </TouchableOpacity>
+
+      {/* Name Wrapper */}
+      <View style={{marginBottom: 26}}>
+        <Controller
+          name='fullName'
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Name is required',
+            },
+            minLength: {
+              value: 3,
+              message: 'Name must be atleast 3 characters long',
+            },
+            maxLength: {
+              value: 50,
+              message: 'Name cannot exceed 50 characters',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <CustomTextInputField
+              label='Name'
+              placeholder='Enter your name'
+              maxLength={51}
+              autoCapitalize='words'
+              value={value}
+              onChangeText={onChange}
+              isValid={errors.fullName ? false : true}
+              errorMessage={errors.fullName ? errors.fullName.message : ''}
+            />
+          )}
+        />
+      </View>
+
+      {/* UserName Wrapper */}
+      <View style={{marginBottom: 26}}>
+        <Controller
+          name='username'
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Username is required',
+            },
+            minLength: {
+              value: 4,
+              message: 'Username must be atleast 3 characters long',
+            },
+            maxLength: {
+              value: 50,
+              message: 'Username cannot exceed 50 characters',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <CustomTextInputField
+              label='Username'
+              placeholder='Enter your Username'
+              maxLength={51}
+              autoCapitalize='words'
+              value={value}
+              onChangeText={val => {
+                const formattedUsername = prefixWithAtSymbol(val);
+                onChange(formattedUsername); // Add @ symbol to username
+              }}
+              isValid={errors.username ? false : true}
+              errorMessage={errors.username ? errors.username.message : ''}
+            />
+          )}
+        />
+      </View>
+
+      {/* DOB Wrapper */}
+      <View style={{marginBottom: 26}}>
+        <Controller
+          name='dob'
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Date Of Birth is required',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <DateTimePicker
+              label='Date Of Birth'
+              onSelectDate={val => {
+                onChange(val);
+              }}
+              value={value}
+              isValid={errors.dob ? false : true}
+              errorMessage={errors.dob ? errors.dob.message : ''}
+            />
+          )}
+        />
+      </View>
+
+      {/* Gender Wrapper */}
+      <View style={{marginBottom: 26}}>
+        <Controller
+          name='gender'
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Gender is required',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <GenderPicker
+              label='Gender'
+              selectedGender={value}
+              setSelectedGender={onChange}
+              isValid={errors.gender ? false : true}
+              errorMessage={errors.gender ? errors.gender.message : ''}
+            />
+          )}
+        />
+      </View>
+
+      <View style={{marginTop: 'auto'}}>
+        <PulseEffect>
+          <Button
+            style={styles.submitButton}
+            onPress={handleSubmit(data => {
+              onSubmit({
+                fullName: data.fullName,
+                username: data.username,
+                dob: data.dob as string,
+                gender: data.gender as number,
+              });
+
+              setRegistrationStep(2);
+            })}>
+            Next
+          </Button>
+        </PulseEffect>
+      </View>
+    </View>
+  );
+};
+
+export default RegistrationGeneralDetails;
+
+const styles = StyleSheet.create({
+  submitButton: {
+    height: 48,
+    borderRadius: BUTTON_BORDER_RADIUS,
+    marginBottom: 20,
+  },
+});
--- src/modules/Fan/FanRegistrationDetails/FanRegistrationDetails.tsx
+++ src/modules/Fan/FanRegistrationDetails/FanRegistrationDetails.tsx
@@ -1,5 +1,5 @@
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React, {Dispatch, SetStateAction, useState} from 'react';
+import React, {useState} from 'react';
 
 import PageContainer from '../../../components/PageContainer';
 import GeneralHeader from '../../../components/GeneralHeader';
@@ -7,24 +7,10 @@ import {useTextColor} from '../../../utils/customHooks/colorHooks';
 import {Button} from 'native-base';
 import {Controller, useForm} from 'react-hook-form';
 import {BUTTON_BORDER_RADIUS} from '../../../constants/styles';
-import {CustomTextInputField} from '../../../components/CustomInputField';
-import {CameraIcon, UserPlaceholderIcon} from '../../../assets/icons';
-import {DateTimePicker} from '../../../components/DateTimePicker';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import {GenderPicker} from '../../../components/GenderPicker';
 import {appColors} from '../../../constants/colors';
 import {Image as ImageType} from 'react-native-image-crop-picker';
-import {
-  openImageCamera,
-  openImagePicker,
-} from '../../../utils/helpers/mediaPicker';
 import {useUploadImageMutation} from '../../../store/postFeed/postFeed.service';
 import {CloudinaryUploadPresets} from '../../../constants/cloudinary';
-import {
-  useAppDispatch,
-  useAppSelector,
-} from '../../../utils/customHooks/storeHooks';
-import {prefixWithAtSymbol} from '../../../utils/helpers/string';
 import {useRegisterFanMutation} from '../../../store/fan/fan.service';
 import {useAppNavigation} from '../../../utils/customHooks/navigator';
 import {PulseEffect} from '../../../components/PulseEffect';
@@ -32,8 +18,9 @@ import {SportsPreferenceSelector} from '../../../components/SportsPreferenceSele
 import {fontBold} from '../../../styles/fonts';
 import {RegisterFanParams} from '../../../types/fan/fan.params';
 import {RecommendationsPage} from '../Recommendations';
+import {RegistrationGeneralDetails} from '../../../components/RegistrationGeneralDetails';
 
-type RegisterFanFormData = {
+export type RegisterFanFormData = {
   profilePic?: ImageType | null;
   fullName?: string;
   username?: string;
@@ -47,6 +34,8 @@ const FanRegistrationDetails = () => {
 
   const [registrationStep, setRegistrationStep] = useState(1);
   const [formData, setFormData] = useState<RegisterFanFormData>({});
+
+  const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
 
   const textColor = useTextColor();
 
@@ -78,15 +67,22 @@ const FanRegistrationDetails = () => {
             switch (registrationStep) {
               case 1:
                 return (
-                  <GeneralDetails
+                  <RegistrationGeneralDetails
                     formData={formData}
                     setFormData={setFormData}
                     setRegistrationStep={setRegistrationStep}
+                    selectedImage={selectedImage}
+                    setSelectedImage={setSelectedImage}
                   />
                 );
 
               case 2:
-                return <ChooseSportsInterest formData={formData} />;
+                return (
+                  <ChooseSportsInterest
+                    formData={formData}
+                    selectedImage={selectedImage}
+                  />
+                );
             }
           })()}
         </View>
@@ -95,278 +91,14 @@ const FanRegistrationDetails = () => {
   );
 };
 
-type GeneralDetailsFormData = {
-  fullName: string;
-  username: string;
-  dob: string;
-  gender: number;
-};
-
-type GeneralDetailsProps = {
-  setRegistrationStep: Dispatch<SetStateAction<number>>;
-  setFormData: Dispatch<SetStateAction<RegisterFanFormData>>;
-  formData: RegisterFanFormData;
-};
-
-const GeneralDetails: React.FC<GeneralDetailsProps> = ({
-  setRegistrationStep,
-  setFormData,
-}) => {
-  const {user} = useAppSelector(state => state.auth);
-
-  const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
-
-  const textColor = useTextColor();
-
-  const {
-    handleSubmit,
-    control,
-
-    formState: {errors},
-  } = useForm({
-    defaultValues: {
-      fullName: user?.fullName || '',
-      username: user?.username || '',
-      dob: undefined as string | undefined,
-      gender: undefined as number | undefined,
-    },
-  });
-
-  const onSubmit = async (data: GeneralDetailsFormData) => {
-    try {
-      console.log('data', data);
-      setFormData(prev => ({
-        ...prev,
-        ...data,
-        profilePic: selectedImage,
-      }));
-      setRegistrationStep(2);
-      return;
-    } catch (error) {
-      console.log(
-        '-------xxxxxx----------Error while Fan details : FanRegistrationDetails.tsx',
-        error,
-      );
-    }
-  };
-
-  const selectImage = async ({
-    useCamera = false, // By Default Gallery will open
-  }: {
-    useCamera?: boolean;
-  }) => {
-    const image = useCamera
-      ? await openImageCamera()
-      : await openImagePicker(false);
-
-    if (image.length > 0) {
-      setSelectedImage(image[0]);
-    }
-  };
-
-  return (
-    <View style={{flex: 1}}>
-      {/* Profile Pic  */}
-      <TouchableOpacity
-        onPress={async () => {
-          await selectImage({useCamera: false});
-        }}
-        activeOpacity={0.5}>
-        <View
-          style={{
-            marginBottom: 26,
-
-            alignSelf: 'center',
-            width: 160,
-            height: 160,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <View
-            style={{
-              position: 'absolute',
-              right: 16,
-              bottom: 16,
-              zIndex: 1,
-              borderRadius: 12,
-              paddingHorizontal: 8,
-              paddingVertical: 8,
-              backgroundColor: appColors.warmRed,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <CameraIcon width={16} height={16} color={appColors.white} />
-          </View>
-
-          <View
-            style={{
-              backgroundColor: appColors.whisperGray,
-              width: 140,
-              height: 140,
-              borderRadius: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-              overflow: 'hidden',
-            }}>
-            {selectedImage === null ? (
-              <UserPlaceholderIcon width={100} height={100} color={textColor} />
-            ) : (
-              <Image
-                source={{uri: selectedImage.path}}
-                style={{width: 140, height: 140}}
-              />
-            )}
-          </View>
-        </View>
-      </TouchableOpacity>
-
-      {/* Name Wrapper */}
-      <View style={{marginBottom: 26}}>
-        <Controller
-          name='fullName'
-          control={control}
-          rules={{
-            required: {
-              value: true,
-              message: 'Name is required',
-            },
-            minLength: {
-              value: 3,
-              message: 'Name must be atleast 3 characters long',
-            },
-            maxLength: {
-              value: 50,
-              message: 'Name cannot exceed 50 characters',
-            },
-          }}
-          render={({field: {onChange, onBlur, value}}) => (
-            <CustomTextInputField
-              label='Name'
-              placeholder='Enter your name'
-              maxLength={51}
-              autoCapitalize='words'
-              value={value}
-              onChangeText={onChange}
-              isValid={errors.fullName ? false : true}
-              errorMessage={errors.fullName ? errors.fullName.message : ''}
-            />
-          )}
-        />
-      </View>
-
-      {/* UserName Wrapper */}
-      <View style={{marginBottom: 26}}>
-        <Controller
-          name='username'
-          control={control}
-          rules={{
-            required: {
-              value: true,
-              message: 'Username is required',
-            },
-            minLength: {
-              value: 4,
-              message: 'Username must be atleast 3 characters long',
-            },
-            maxLength: {
-              value: 50,
-              message: 'Username cannot exceed 50 characters',
-            },
-          }}
-          render={({field: {onChange, onBlur, value}}) => (
-            <CustomTextInputField
-              label='Username'
-              placeholder='Enter your Username'
-              maxLength={51}
-              autoCapitalize='words'
-              value={value}
-              onChangeText={val => {
-                const formattedUsername = prefixWithAtSymbol(val);
-                onChange(formattedUsername); // Add @ symbol to username
-              }}
-              isValid={errors.username ? false : true}
-              errorMessage={errors.username ? errors.username.message : ''}
-            />
-          )}
-        />
-      </View>
-
-      {/* DOB Wrapper */}
-      <View style={{marginBottom: 26}}>
-        <Controller
-          name='dob'
-          control={control}
-          rules={{
-            required: {
-              value: true,
-              message: 'Date Of Birth is required',
-            },
-          }}
-          render={({field: {onChange, onBlur, value}}) => (
-            <DateTimePicker
-              label='Date Of Birth'
-              onSelectDate={val => {
-                onChange(val);
-              }}
-              value={value}
-              isValid={errors.dob ? false : true}
-              errorMessage={errors.dob ? errors.dob.message : ''}
-            />
-          )}
-        />
-      </View>
-
-      {/* Gender Wrapper */}
-      <View style={{marginBottom: 26}}>
-        <Controller
-          name='gender'
-          control={control}
-          rules={{
-            required: {
-              value: true,
-              message: 'Gender is required',
-            },
-          }}
-          render={({field: {onChange, onBlur, value}}) => (
-            <GenderPicker
-              label='Gender'
-              selectedGender={value}
-              setSelectedGender={onChange}
-              isValid={errors.gender ? false : true}
-              errorMessage={errors.gender ? errors.gender.message : ''}
-            />
-          )}
-        />
-      </View>
-
-      <View style={{marginTop: 'auto'}}>
-        <PulseEffect>
-          <Button
-            style={styles.submitButton}
-            onPress={handleSubmit(data => {
-              onSubmit({
-                fullName: data.fullName,
-                username: data.username,
-                dob: data.dob as string,
-                gender: data.gender as number,
-              });
-
-              setRegistrationStep(2);
-            })}>
-            Next
-          </Button>
-        </PulseEffect>
-      </View>
-    </View>
-  );
-};
-
 type ChooseSportsInterestProps = {
   formData: RegisterFanFormData;
+  selectedImage: ImageType | null;
 };
 
 const ChooseSportsInterest: React.FC<ChooseSportsInterestProps> = ({
   formData,
+  selectedImage,
 }) => {
   const navigation = useAppNavigation();
   const {
@@ -385,10 +117,10 @@ const ChooseSportsInterest: React.FC<ChooseSportsInterestProps> = ({
   const [registerFan, {isLoading: registerFanCIP}] = useRegisterFanMutation();
 
   const uploadToCloudinary = async () => {
-    if (formData.profilePic) {
+    if (selectedImage) {
       try {
         const uploadedImage = await uploadImagesToCloudinary({
-          imageDataBase64: `data:image/jpg;base64,${formData.profilePic.data}`,
+          imageDataBase64: `data:image/jpg;base64,${selectedImage.data}`,
           uploadPreset: CloudinaryUploadPresets.PROFILE_PICS,
         }).unwrap();
 
@@ -459,7 +191,6 @@ const ChooseSportsInterest: React.FC<ChooseSportsInterestProps> = ({
             <SportsPreferenceSelector
               selectedSports={value || []}
               onSportsSelected={sports => {
-                console.log('sports', sports);
                 onChange(sports);
               }}
               isValid={errors.sportInterests ? false : true}
@@ -475,15 +206,7 @@ const ChooseSportsInterest: React.FC<ChooseSportsInterestProps> = ({
         <PulseEffect>
           <Button
             style={styles.submitButton}
-            onPress={handleSubmit(data => {
-              onSubmit({
-                // fullName: data.fullName,
-                // username: data.username,
-                // dob: data.dob as string,
-                // gender: data.gender as number,
-                sportInterests: data.sportInterests,
-              });
-            })}
+            onPress={handleSubmit(onSubmit)}
             isLoading={imageUploadCIP || registerFanCIP}>
             Done
           </Button>
