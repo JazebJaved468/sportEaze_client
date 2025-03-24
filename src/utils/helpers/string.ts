07adb3e09c6a6b47b751f24b5268c216abff6e0a
M Jazeb Javed <jazebjaved52@gmail.com>
2025-03-24 07:20:26 +0500

    feat: enhance CreatePost component and add profile URL validation helpers

--- src/utils/helpers/string.ts
+++ src/utils/helpers/string.ts
@@ -2,3 +2,34 @@ export const prefixWithAtSymbol = (username: string): string => {
   if (!username) return '@'; // Handle empty input
   return username.startsWith('@') ? username : `@${username}`;
 };
+
+export const isValidFacebookProfileUrl = (url: string) => {
+  const facebookRegex =
+    /^(https?:\/\/)?(www\.)?facebook\.com\/[a-zA-Z0-9.]+\/?$/;
+
+  return (
+    facebookRegex.test(url) || 'Please enter a valid Facebook profile URL.'
+  );
+};
+
+export const isValidInstagramProfileUrl = (url: string) => {
+  const instagramRegex = /^https:\/\/www\.instagram\.com\/[a-zA-Z0-9_.]+\/?$/;
+
+  return (
+    instagramRegex.test(url) || 'Please enter a valid Instagram profile URL.'
+  );
+};
+
+export const isValidLinkedinProfileUrl = (url: string) => {
+  const linkedinRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9-]+\/?$/;
+
+  return (
+    linkedinRegex.test(url) || 'Please enter a valid Linkedin profile URL.'
+  );
+};
+
+export const isValidTwitterProfileUrl = (url: string) => {
+  const twitterRegex = /^https:\/\/(www\.)?twitter\.com\/[a-zA-Z0-9_]+\/?$/;
+
+  return twitterRegex.test(url) || 'Please enter a valid X profile URL.';
+};
