897c1f93630f08dc572c5fec355dca421f743b46
M Jazeb Javed <jazebjaved52@gmail.com>
2025-05-12 13:48:56 +0500

    feat: add splash screen with gradient background and logo

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,20 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import React from 'react';
-import {SportEazeLogo} from '../../assets/icons';
 import {appColors} from '../../constants/colors';
+import LinearGradient from 'react-native-linear-gradient';
+import {screenHeight, screenWidth} from '../../constants/styles';
+import {useColorModeValue} from 'native-base';
+import PageContainer from '../PageContainer';
+import {customWidth} from '../../styles/responsiveStyles';
 
 export const SplashScreen = () => {
+  const shadowColor = useColorModeValue(appColors.black, appColors.white);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: appColors.white,
-      }}>
-      <SportEazeLogo color={appColors.black} />
-    </View>
+    <PageContainer>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: appColors.white,
+        }}>
+        <View style={styles.shadowContainer}>
+          <LinearGradient
+            start={{x: 0.5, y: 5}} // Start from bottom center
+            end={{x: 0.5, y: 0}}
+            colors={[shadowColor, appColors.transparent]}>
+            <View
+              style={{
+                width: screenWidth,
+                height: screenHeight,
+              }}
+            />
+          </LinearGradient>
+        </View>
+
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={require('../../assets/images/splash_logo_duo.png')}
+            style={{
+              width: screenWidth - customWidth(130),
+              objectFit: 'contain',
+            }}
+          />
+        </View>
+      </View>
+    </PageContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  shadowContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: -1,
+  },
+});
--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,1 +1,6 @@
+import {Dimensions} from 'react-native';
+
 export const BUTTON_BORDER_RADIUS = 18;
+
+export const screenWidth = Dimensions.get('window').width;
+export const screenHeight = Dimensions.get('window').height;
