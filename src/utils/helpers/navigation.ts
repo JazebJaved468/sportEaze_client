e5b82a3045e7848217435ada73dbc8635024d9dc
M Jazeb Javed <jazebjaved52@gmail.com>
2025-05-15 01:41:42 +0500

    feat: add navigation support for Mentor and Patron profiles and registration details

--- src/utils/helpers/navigation.ts
+++ src/utils/helpers/navigation.ts
@@ -5,6 +5,10 @@ import {USER_TYPE} from '../../constants/enums';
 import {FanProfilePage} from '../../modules/Fan/FanProfile';
 import {PlayerRegistrationDetailsPage} from '../../modules/Player/PlayerRegistrationDetails';
 import {FanRegistrationDetailsPage} from '../../modules/Fan/FanRegistrationDetails';
+import {MentorProfilePage} from '../../modules/Mentor/MentorProfile';
+import {MentorRegistrationDetailsPage} from '../../modules/Mentor/MentorRegistrationDetails.tsx';
+import {PatronProfilePage} from '../../modules/Patron/PatronProfile/index.ts';
+import {PatronRegistrationDetailsPage} from '../../modules/Patron/PatronRegistrationDetails/index.ts';
 
 export const navigationRef = createNavigationContainerRef<RootStackParamList>();
 
@@ -25,6 +29,14 @@ export const navigateToProfilePage = ({
       navigationRef.navigate(FanProfilePage, {
         userId: userId,
       });
+    } else if (userType === USER_TYPE.MENTOR) {
+      navigationRef.navigate(MentorProfilePage, {
+        userId: userId,
+      });
+    } else if (userType === USER_TYPE.PATRON) {
+      navigationRef.navigate(PatronProfilePage, {
+        userId: userId,
+      });
     }
   }
 };
@@ -35,6 +47,14 @@ export const navigateToEditProfilePage = ({userType}: {userType: number}) => {
       navigationRef.navigate(PlayerRegistrationDetailsPage);
     } else if (userType === USER_TYPE.FAN) {
       navigationRef.navigate(FanRegistrationDetailsPage, {isEditProfile: true});
+    } else if (userType === USER_TYPE.MENTOR) {
+      navigationRef.navigate(MentorRegistrationDetailsPage, {
+        isEditProfile: true,
+      });
+    } else if (userType === USER_TYPE.PATRON) {
+      navigationRef.navigate(PatronRegistrationDetailsPage, {
+        isEditProfile: true,
+      });
     }
   }
 };
