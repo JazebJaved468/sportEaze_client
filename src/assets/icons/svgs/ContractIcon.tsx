0a706231649cfc33001e8115694da811c911581b
M Jazeb Javed <jazebjaved52@gmail.com>
2025-05-15 12:35:43 +0500

    fix: allow customizable stroke width in ContractIcon component

--- src/assets/icons/svgs/ContractIcon.tsx
+++ src/assets/icons/svgs/ContractIcon.tsx
@@ -11,14 +11,14 @@ export const ContractIcon = (props: SvgProps) => (
       stroke={props.color ?? '#000'}
       strokeLinecap='round'
       strokeLinejoin='round'
-      strokeWidth={1.5}
+      strokeWidth={props.strokeWidth ?? 1.5}
       d='M22 10v5c0 5-2 7-7 7H9c-5 0-7-2-7-7V9c0-5 2-7 7-7h5'
     />
     <Path
       stroke={props.color ?? '#000'}
       strokeLinecap='round'
       strokeLinejoin='round'
-      strokeWidth={1.5}
+      strokeWidth={props.strokeWidth ?? 1.5}
       d='M22 10h-4c-3 0-4-1-4-4V2l8 8ZM7 13h6M7 17h4'
     />
   </Svg>
