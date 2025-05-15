4de89225d8b4fd0aba6e37e9872e546a4b7b35e5
Mahnoor Waseem <mahnoorwaseem60@gmail.com>
2025-05-16 04:35:26 +0500

    added profile data

---
+++ src/modules/Patron/Profile/index.ts
@@ -0,0 +1,4 @@
+import {PatronProfile} from './PatronProfile';
+export default PatronProfile;
+
+export const PatronProfilePage = 'PatronProfilePage';
---
+++ src/modules/Patron/Profile/mockProfileData.ts
@@ -0,0 +1,28 @@
+const mockProfileData = {
+    name: "Emma Philips",
+    title:"Global Sports Equipment & Apparel",
+    location: "Karachi, Pakistan",
+    coverImage:
+      "https://i.pinimg.com/736x/e6/36/16/e636169105598b729fb60bef20004faa.jpg", // Horizontal Unsplash cover image
+    profileImage:
+      "https://i.redd.it/which-elena-gilbert-hairstyle-do-you-think-looked-the-best-v0-kij6e9rmkf7d1.png?width=564&format=png&auto=webp&s=b4e04e026627a55907a73d2efdacb068d15991f4", // Random user profile image
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+    telephone: "123-46-7889",
+    email: "emma@abc.com",
+    packages: [
+      {
+        title: "Basic Package",
+        price: "Rs. 25,000",
+        features: ["Full equipment sponsorship with monetary support", "Package Feature 2"],
+      },
+      {
+        title: "Silver Package",
+        price: "Rs. 50,000",
+        features: ["Package Feature 1", "Package Feature 2", "Package Feature 3"],
+      },
+    ],
+  };
+  
+  export default mockProfileData;
+  
